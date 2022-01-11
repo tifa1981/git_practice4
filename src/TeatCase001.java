1dcfe3d93fbfe8088756ab59e30ea17e018db155
YanchunZhou <tifa1981@gmail.com>
2022-01-11 12:56:30 -0500

    Initial commit

---
+++ src/TeatCase001.java
@@ -0,0 +1,9 @@
+public class TeatCase001 {
+    public static void main(String[] args) {
+        System.out.println("fetch");
+
+        // If you have accidentally tracked .iml file do this
+        //git rm --cached git_practice2.iml
+        //and commit and push
+    }
+}
