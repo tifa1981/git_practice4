a30121dba339a2be15f71b61b2a109d8d3111412
YanchunZhou <tifa1981@gmail.com>
2022-01-18 11:36:08 -0500

    day 3 commit

--- src/TeatCase001.java
+++ src/TeatCase001.java
@@ -1,9 +1,11 @@
 public class TeatCase001 {
-    public static void main(String[] args) {
-        System.out.println("fetch");
+
 
         // If you have accidentally tracked .iml file do this
         //git rm --cached git_practice2.iml
         //and commit and push
+    //https://github.com/tifa1981/git_practice4.git
+
+
     }
-}
+
